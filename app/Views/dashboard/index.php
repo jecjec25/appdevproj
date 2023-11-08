f61f6404c9899291d552e3491656618660b58aa3
Ramirez, Jennifer M <144778693+jen2112@users.noreply.github.com>
2023-11-08 10:33:44 +0800

    main

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -85,7 +85,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/form">
+            <a class="nav-link" href="pages/forms/basic_elements.html">
               <i class="mdi mdi-format-list-bulleted menu-icon"></i>
               <span class="menu-title">Forms</span>
             </a>
@@ -140,75 +140,6 @@
               <i class="mdi mdi-menu"></i>
             </button>
             <ul class="navbar-nav">
-              <li class="nav-item dropdown">
-                <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-                  <i class="mdi mdi-bell-outline"></i>
-                  <span class="count count-varient1">7</span>
-                </a>
-                <div class="dropdown-menu navbar-dropdown navbar-dropdown-large preview-list" aria-labelledby="notificationDropdown">
-                  <h6 class="p-3 mb-0">Notifications</h6>
-                  <a class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                      <img src="assets/images/faces/face4.jpg" alt="" class="profile-pic" />
-                    </div>
-                    <div class="preview-item-content">
-                      <p class="mb-0"> Dany Miles <span class="text-small text-muted">commented on your photo</span>
-                      </p>
-                    </div>
-                  </a>
-                  <a class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                      <img src="assets/images/faces/face3.jpg" alt="" class="profile-pic" />
-                    </div>
-                    <div class="preview-item-content">
-                      <p class="mb-0"> James <span class="text-small text-muted">posted a photo on your wall</span>
-                      </p>
-                    </div>
-                  </a>
-                  <a class="dropdown-item preview-item">
-                    <div class="preview-thumbnail">
-                      <img src="assets/images/faces/face2.jpg" alt="" class="profile-pic" />
-                    </div>
-                    <div class="preview-item-content">
-                      <p class="mb-0"> Alex <span class="text-small text-muted">just mentioned you in his post</span>
-                      </p>
-                    </div>
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <p class="p-3 mb-0">View all activities</p>
-                </div>
-              </li>
-              <li class="nav-item dropdown d-none d-sm-flex">
-                <a class="nav-link count-indicator dropdown-toggle" id="messageDropdown" href="#" data-toggle="dropdown">
-                  <i class="mdi mdi-email-outline"></i>
-                  <span class="count count-varient2">5</span>
-                </a>
-                <div class="dropdown-menu navbar-dropdown navbar-dropdown-large preview-list" aria-labelledby="messageDropdown">
-                  <h6 class="p-3 mb-0">Messages</h6>
-                  <a class="dropdown-item preview-item">
-                    <div class="preview-item-content flex-grow">
-                      <span class="badge badge-pill badge-success">Request</span>
-                      <p class="text-small text-muted ellipsis mb-0"> Suport needed for user123 </p>
-                    </div>
-                    <p class="text-small text-muted align-self-start"> 4:10 PM </p>
-                  </a>
-                  <a class="dropdown-item preview-item">
-                    <div class="preview-item-content flex-grow">
-                      <span class="badge badge-pill badge-warning">Invoices</span>
-                      <p class="text-small text-muted ellipsis mb-0"> Invoice for order is mailed </p>
-                    </div>
-                    <p class="text-small text-muted align-self-start"> 4:10 PM </p>
-                  </a>
-                  <a class="dropdown-item preview-item">
-                    <div class="preview-item-content flex-grow">
-                      <span class="badge badge-pill badge-danger">Projects</span>
-                      <p class="text-small text-muted ellipsis mb-0"> New project will start tomorrow </p>
-                    </div>
-                    <p class="text-small text-muted align-self-start"> 4:10 PM </p>
-                  </a>
-                  <h6 class="p-3 mb-0">See all activity</h6>
-                </div>
-              </li>
               <li class="nav-item nav-search border-0 ml-1 ml-md-3 ml-lg-5 d-none d-md-flex">
                 <form class="nav-link form-inline mt-2 mt-md-0">
                   <div class="input-group">
@@ -223,20 +154,10 @@
               </li>
             </ul>
             <ul class="navbar-nav navbar-nav-right ml-lg-auto">
-              <li class="nav-item dropdown d-none d-xl-flex border-0">
-                <a class="nav-link dropdown-toggle" id="languageDropdown" href="#" data-toggle="dropdown">
-                  <i class="mdi mdi-earth"></i> English </a>
-                <div class="dropdown-menu navbar-dropdown" aria-labelledby="languageDropdown">
-                  <a class="dropdown-item" href="#"> French </a>
-                  <a class="dropdown-item" href="#"> Spain </a>
-                  <a class="dropdown-item" href="#"> Latin </a>
-                  <a class="dropdown-item" href="#"> Japanese </a>
-                </div>
-              </li>
               <li class="nav-item nav-profile dropdown border-0">
                 <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-toggle="dropdown">
                   <img class="nav-profile-img mr-2" alt="" src="assets/images/faces/face1.jpg" />
-                  <span class="profile-name">Henry Klein</span>
+                  <span class="profile-name">Admin</span>
                 </a>
                 <div class="dropdown-menu navbar-dropdown w-100" aria-labelledby="profileDropdown">
                   <a class="dropdown-item" href="#">
@@ -255,29 +176,7 @@
 
 
         <div class="main-panel">
-            <br>
-        <?php include('financesert.php')?>
-          <div class="tab-content">
-            <div id="approved" class="tab-pane active in">
-                <table id="table" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>Fiscal Year</th>
-                            <th>Annual Regular Income</th>
-                            <th>Change</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php foreach ($fin as $St): ?>
-                    <th><?= $St['FiscalYear'];?></th>
-                    <th><?= $St['AnnualRegularIncome']?></th>
-                    <th><?=  $St['Change'];  echo('%')?></th>
-
-                    </tbody>
-                    <?php endforeach;?>
-                </table>
-
-                </div>
+        <div class="content-wrapper">
           <div class="content-wrapper pb-0">
             <div class="page-header flex-wrap">
               <h3 class="mb-0"> Hi, welcome back! <span class="pl-0 h6 pl-sm-2 text-muted d-inline-block">Your web analytics dashboard template.</span>
@@ -298,13 +197,13 @@
                       <div class="card-body px-3 py-4">
                         <div class="d-flex justify-content-between align-items-start">
                           <div class="color-card">
-                            <p class="mb-0 color-card-head">Sales</p>
-                            <h2 class="text-white"> $8,753.<span class="h5">00</span>
+                            <p class="mb-0 color-card-head">Barangay's in Calapan City</p>
+                            <h2 class="text-white"> $8,<span class="h5">00</span>
                             </h2>
                           </div>
                           <i class="card-icon-indicator mdi mdi-basket bg-inverse-icon-warning"></i>
                         </div>
-                        <h6 class="text-white">18.33% Since last month</h6>
+    
                       </div>
                     </div>
                   </div>
@@ -313,13 +212,13 @@
                       <div class="card-body px-3 py-4">
                         <div class="d-flex justify-content-between align-items-start">
                           <div class="color-card">
-                            <p class="mb-0 color-card-head">Margin</p>
+                            <p class="mb-0 color-card-head">Mayor Councilor</p>
                             <h2 class="text-white"> $5,300.<span class="h5">00</span>
                             </h2>
                           </div>
                           <i class="card-icon-indicator mdi mdi-cube-outline bg-inverse-icon-danger"></i>
                         </div>
-                        <h6 class="text-white">13.21% Since last month</h6>
+                       
                       </div>
                     </div>
                   </div>
@@ -328,13 +227,13 @@
                       <div class="card-body px-3 py-4">
                         <div class="d-flex justify-content-between align-items-start">
                           <div class="color-card">
-                            <p class="mb-0 color-card-head">Orders</p>
-                            <h2 class="text-white"> $1,753.<span class="h5">00</span>
+                            <p class="mb-0 color-card-head">Nurse in each Barangay</p>
+                            <h2 class="text-white"> $1,<span class="h5">00</span>
                             </h2>
                           </div>
                           <i class="card-icon-indicator mdi mdi-briefcase-outline bg-inverse-icon-primary"></i>
                         </div>
-                        <h6 class="text-white">67.98% Since last month</h6>
+                        
                       </div>
                     </div>
                   </div>
@@ -343,12 +242,12 @@
                       <div class="card-body px-3 py-4">
                         <div class="d-flex justify-content-between align-items-start">
                           <div class="color-card">
-                            <p class="mb-0 color-card-head">Affiliate</p>
+                            <p class="mb-0 color-card-head">Staff's</p>
                             <h2 class="text-white">2368</h2>
                           </div>
                           <i class="card-icon-indicator mdi mdi-account-circle bg-inverse-icon-success"></i>
                         </div>
-                        <h6 class="text-white">20.32% Since last month</h6>
+                        
                       </div>
                     </div>
                   </div>
@@ -440,15 +339,13 @@
               <div class="col-xl-8 col-sm-6 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body px-0 overflow-auto">
-                    <h4 class="card-title pl-4">Purchase History</h4>
+                    <h4 class="card-title pl-4">Barangay Information</h4>
                     <div class="table-responsive">
                       <table class="table">
                         <thead class="bg-light">
                           <tr>
-                            <th>Customer</th>
-                            <th>Project</th>
-                            <th>Invoice</th>
-                            <th>Amount</th>
+                            <th>Barangay Chairman</th>
+                            <th>Barangay</th>
                           </tr>
                         </thead>
                         <tbody>
